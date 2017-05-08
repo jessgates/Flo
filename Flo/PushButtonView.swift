cb5fc95ec1f2a97eb65b1ab37b8f33f3f2429833
jessgates <gates.jess@gmail.com>
2017-05-08 16:09:07 -0500

    Chore:
    Initial commit

---
+++ Flo/PushButtonView.swift
@@ -0,0 +1,21 @@
+//
+//  PushButtonView.swift
+//  Flo
+//
+//  Created by Jess Gates on 5/5/17.
+//  Copyright © 2017 Jess Gates. All rights reserved.
+//
+
+import UIKit
+
+class PushButtonView: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
