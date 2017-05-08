ccc21e1558e83eb6f525d789fb214f385d836d31
jessgates <gates.jess@gmail.com>
2017-05-08 16:11:38 -0500

    Feature: Add Button
    Create add button

--- Flo/PushButtonView.swift
+++ Flo/PushButtonView.swift
@@ -7,15 +7,36 @@
 //
 
 import UIKit
+@IBDesignable
 
 class PushButtonView: UIButton {
+    
+    @IBInspectable var fillColor: UIColor = .green
+    @IBInspectable var isAddButton: Bool = true
 
-    /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
         // Drawing code
+        var path = UIBezierPath(ovalIn: rect)
+        fillColor.setFill()
+        path.fill()
+        
+        let plusHeight: CGFloat = 3.0
+        let plusWidth: CGFloat = min(bounds.width, bounds.height) * 0.6
+        
+        var plusPath = UIBezierPath()
+        plusPath.lineWidth = plusHeight
+        
+        plusPath.move(to: CGPoint(x: bounds.width/2 - plusWidth/2 + 0.5, y: bounds.height/2 + 0.5))
+        
+        plusPath.addLine(to: CGPoint(x:bounds.width/2 + plusWidth/2 + 0.5, y:bounds.height/2 + 0.5))
+        
+        plusPath.move(to: CGPoint(x:bounds.width/2 + 0.5, y:bounds.height/2 - plusWidth/2 + 0.5))
+        
+        plusPath.addLine(to: CGPoint(x:bounds.width/2 + 0.5, y:bounds.height/2 + plusWidth/2 + 0.5))
+        
+        UIColor.white.setStroke()
+        plusPath.stroke()
     }
-    */
-
 }
