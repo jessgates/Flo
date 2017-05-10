10e82e56c3df6706b2080044325a64550b11fcb4
jessgates <gates.jess@gmail.com>
2017-05-10 14:21:55 -0500

    Feature: Add Points to graph

--- Flo/GraphView.swift
+++ Flo/GraphView.swift
@@ -13,6 +13,8 @@ import UIKit
     @IBInspectable var startColor: UIColor = .red
     @IBInspectable var endColor: UIColor = .green
     
+    var graphPoints:[Int] = [4, 2, 6, 4, 5, 8, 3]
+    
     override func draw(_ rect: CGRect) {
         let width = rect.width
         let height = rect.height
@@ -31,6 +33,44 @@ import UIKit
         
         var startPoint = CGPoint.zero
         var endPoint = CGPoint(x: 0.0, y: bounds.height)
-        context!.drawLinearGradient(gradient!, start: startPoint, end: endPoint, options: [])}
+        context!.drawLinearGradient(gradient!, start: startPoint, end: endPoint, options: [])
+        
+        let margin: CGFloat = 20.0
+        var columnXpoint = { (column:Int) -> CGFloat in
+            let spacer = (width - margin * 2 - 4) / CGFloat((self.graphPoints.count - 1))
+            var x:CGFloat = CGFloat(column) * spacer
+            x += margin + 2
+            return x
+        }
+        
+        let topBorder:CGFloat = 60
+        let bottomBorder:CGFloat = 50
+        let graphHeight = height - topBorder - bottomBorder
+        let maxValue = graphPoints.max()
+        var columnYPoint = { (graphPoint:Int) -> CGFloat in
+            var y:CGFloat = CGFloat(graphPoint) /
+                CGFloat(maxValue!) * graphHeight
+            y = graphHeight + topBorder - y // Flip the graph
+            return y
+        }
+        
+        UIColor.white.setFill()
+        UIColor.white.setStroke()
+        
 
+        var graphPath = UIBezierPath()
+
+        graphPath.move(to: CGPoint(x:columnXpoint(0),
+                                      y:columnYPoint(graphPoints[0])))
+        
+        //add points for each item in the graphPoints array
+        //at the correct (x, y) for the point
+        for i in 1..<graphPoints.count {
+            let nextPoint = CGPoint(x:columnXpoint(i),
+                                    y:columnYPoint(graphPoints[i]))
+            graphPath.addLine(to: nextPoint)
+        }
+        
+        graphPath.stroke()
+    }
 }
